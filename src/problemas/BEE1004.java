533bef342cef5959556f062ee889c5bc3e7a39b9
josecampelo <josecampelojr@gmail.com>
2024-10-17 01:04:37 -0300

    Resolução do problema 1004 concluído

---
+++ src/problemas/BEE1004.java
@@ -0,0 +1,20 @@
+package problemas;
+
+import java.util.Scanner;
+
+// Produto Simples
+public class BEE1004 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a, b, prod;
+
+        a = entrada.nextInt();
+        b = entrada.nextInt();
+
+        prod = a * b;
+
+        System.out.println("PROD = " + prod);
+
+        entrada.close();
+    }
+}
