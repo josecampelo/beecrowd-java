f693479e7b2e9b81e0cff50b65aeb16d022e1d93
josecampelo <josecampelojr@gmail.com>
2024-10-17 00:41:35 -0300

    Resolução do problema 1002 concluído

---
+++ src/problemas/BEE1002.java
@@ -0,0 +1,17 @@
+package problemas;
+
+import java.util.Scanner;
+
+public class BEE1002 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double n, raio, area;
+
+        n = 3.14159;
+        raio = entrada.nextDouble();
+
+        area = raio * raio * n;
+
+        System.out.printf("A=%.4f%n", area);
+    }
+}
