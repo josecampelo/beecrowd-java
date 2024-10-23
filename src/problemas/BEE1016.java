0de5706246faee2d49b80a9529fa30aec54bd4a6
josecampelo <josecampelojr@gmail.com>
2024-10-22 23:40:22 -0300

    Resolução do problema 1016

---
+++ src/problemas/BEE1016.java
@@ -0,0 +1,13 @@
+package problemas;
+
+import java.util.Scanner;
+
+// - Distância
+public class BEE1016 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+
+        entrada.close();
+    }
+}
