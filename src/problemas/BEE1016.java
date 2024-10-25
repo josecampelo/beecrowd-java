e8431ad35dbb0ebb9f7e3b6e3372a78762f602a3
josecampelo <josecampelojr@gmail.com>
2024-10-25 00:25:26 -0300

    Resolução do problema 1016

--- src/problemas/BEE1016.java
+++ src/problemas/BEE1016.java
@@ -6,7 +6,13 @@ import java.util.Scanner;
 public class BEE1016 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        int distancia, tempo;
 
+        distancia = entrada.nextInt();
+
+        tempo = distancia * 2;
+
+        System.out.println(tempo + " minutos");
 
         entrada.close();
     }
